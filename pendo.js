4b90a80affc91c8775539e61b9cd99f0a199b078
BHRunkel19 <brandon.runkel@gmail.com>
2020-12-03 11:05:47 -0500

    moved svc logic to jsfile

--- pendo.js
+++ pendo.js
@@ -113,6 +113,7 @@ var accounts = [
                 ready: function() {
                     console.log("Pendo is ready!")
                     addTrackEvent();
+                    configEmbeddedSvc();
                 },
                 guidesLoaded: function() {
                     console.log("The guides have loaded!")
@@ -144,6 +145,61 @@ var accounts = [
                 })  
             })
         }
+
+        function configEmbeddedSvc(){
+            var initESW = function(gslbBaseURL) {
+                embedded_svc.settings.targetElement = document.querySelector('#toast_sf_chat');
+                embedded_svc.settings.displayHelpButton = false; //Or false
+                embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
+          
+                //embedded_svc.settings.defaultMinimizedText = '...'; //(Defaults to Chat with an Expert)
+                //embedded_svc.settings.disabledMinimizedText = '...'; //(Defaults to Agent Offline)
+          
+                //embedded_svc.settings.loadingText = ''; //(Defaults to Loading)
+                //embedded_svc.settings.storageDomain = 'yourdomain.com'; //(Sets the domain for your deployment so that visitors can navigate subdomains during a chat session)
+          
+                // Settings for Chat
+                //embedded_svc.settings.directToButtonRouting = function(prechatFormData) {
+                  // Dynamically changes the button ID based on what the visitor enters in the pre-chat form.
+                  // Returns a valid button ID.
+                //};
+                //embedded_svc.settings.prepopulatedPrechatFields = {}; //Sets the auto-population of pre-chat form fields
+                //embedded_svc.settings.fallbackRouting = []; //An array of button IDs, user IDs, or userId_buttonId
+                //embedded_svc.settings.offlineSupportMinimizedText = '...'; //(Defaults to Contact Us)
+          
+                embedded_svc.settings.enabledFeatures = ['LiveAgent'];
+                embedded_svc.settings.entryFeature = 'LiveAgent';
+          
+                embedded_svc.init(
+                  'https://toast--trdev.my.salesforce.com',
+                  'https://trdev-toast.cs69.force.com/success',
+                  gslbBaseURL,
+                  '00D2D000000E5U5',
+                  'Toast_Web_Chat_V2',
+                  {
+                    baseLiveAgentContentURL: 'https://c.la4-c1cs-phx.salesforceliveagent.com/content',
+                    deploymentId: '5723c000000k9iJ',
+                    buttonId: '573C00000008RMb',
+                    baseLiveAgentURL: 'https://d.la4-c1cs-phx.salesforceliveagent.com/chat',
+                    eswLiveAgentDevName: 'EmbeddedServiceLiveAgent_Parent04I3c0000000CK9EAM_174b30518e1',
+                    isOfflineSupportEnabled: false
+                  }
+                );
+            };
+          
+            if (!window.embedded_svc) {
+            var s = document.createElement('script');
+            s.setAttribute('src', 'https://toast--trdev.my.salesforce.com/embeddedservice/5.0/esw.min.js');
+            s.onload = function() {
+                initESW(null);
+            };
+            document.body.appendChild(s);
+            } else {
+            initESW('https://service.force.com');
+            }
+            console.log('Configuration of Embedded Service complete');
+        }
+            
 
         function addRCElementHandler(){
             var launchRCElement = document.querySelector('[data-id="1.0-Nav-Home.Dashboard"]');
